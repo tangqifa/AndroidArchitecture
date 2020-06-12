122ec8028a43030e99689f269fb5431f34715a6b
tangqifa <tangqifa@126.com>
2020-06-12 10:24:41 +0800

    init project

---
+++ app/src/main/java/cn/knowbox/androidarchitecture/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.knowbox.androidarchitecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
